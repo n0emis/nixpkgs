07a4caa118155130c9cea7b14bdd9f17731d4ef5
Ember 'n0emis' Keske <git@n0emis.eu>
2022-03-05 19:24:24 +0100

    python3Packages.social-auth-app-django: init at 5.0.0

---
+++ pkgs/development/python-modules/social-auth-app-django/default.nix
@@ -0,0 +1,30 @@
+{ lib, buildPythonPackage, fetchPypi, social-auth-core, django, python }:
+
+buildPythonPackage rec {
+  pname = "social-auth-app-django";
+  version = "5.0.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "b6e3132ce087cdd6e1707aeb1b588be41d318408fcf6395435da0bc6fe9a9795";
+  };
+
+  propagatedBuildInputs = [
+    social-auth-core
+  ];
+
+  checkInputs = [
+    django
+  ];
+
+  checkPhase = ''
+    ${python.interpreter} -m django test --settings="tests.settings"
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/python-social-auth/social-app-django";
+    description = "Python Social Auth - Application - Django";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ n0emis ];
+  };
+}
